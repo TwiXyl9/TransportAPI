4a93719059200b531d3e76b2da9e6772ff7529c1
TwiXyl9 <kolkin.2002@mail.com>
2022-12-05 19:09:38 +0300

    Add Role model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+admin = Role.create!(name: 'admin')
+user = Role.create!(name: 'user')
